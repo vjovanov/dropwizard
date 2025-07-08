f55735bcf1c629a031eea1392d5670599cc30e18
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-07-08 10:59:54 +0200

    Db migration is working

--- dropwizard-example/run-agent.sh
+++ dropwizard-example/run-agent.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
 $GRAALVM_HOME/bin/java -agentlib:native-image-agent=config-merge-dir=agent-metadata -jar target/dropwizard-example-5.0.0-SNAPSHOT.jar db migrate example.yml
+
+$GRAALVM_HOME/bin/java -agentlib:native-image-agent=config-merge-dir=agent-metadata,experimental-class-define-support -jar target/dropwizard-example-5.0.0-SNAPSHOT.jar server example.yml &
+
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+
+
+
--- dropwizard-example/run.sh
+++ dropwizard-example/run.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
 ./dropwizard-example-5.0.0-SNAPSHOT -XX:MissingRegistrationReportingMode=Warn db migrate example.yml
+
+./dropwizard-example-5.0.0-SNAPSHOT -XX:MissingRegistrationReportingMode=Warn server example.yml &
+
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
+curl -H "Content-Type: application/json" -X POST -d '{"fullName":"Other Person","jobTitle":"Other Title"}' http://localhost:8080/people
